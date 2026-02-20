b8336e2d534908fdb9228d71fd0bd89d4809fcb0
SaiNetha <44888832+SaiNetha9966@users.noreply.github.com>
2026-02-20 19:19:44 +0530

    data handling in affboaring

--- src/app/components/OffboardReview/DataHandling.tsx
+++ src/app/components/OffboardReview/DataHandling.tsx
@@ -1,11 +1,32 @@
 import svgPaths from '../../../imports/svg-0ywn1y5h0h';
 
-export function DataHandling() {
+interface DataHandlingProps{
+  selectOffboadingScope:string;
+}
+
+interface ToolItemProps {
+  name: string;
+  status:string;
+}
+function ToolItem({ name,status }: ToolItemProps) {
+  return (
+   <div className="flex flex-col gap-2">
+     <p className="font-bold text-[15px] text-[#4a4a4a]">{name}</p>
+      <div 
+      className={`rounded-full px-2.5 h-6 flex items-center w-fit ${status !== "Delete" ? "bg-[#E0EAFF] border border-[#9BB5FD]" : "bg-[#FFEBED] border border-[#F1B5B7]"}`} >
+         <span className="font-normal text-[12px] leading-none">{status}</span> 
+         </div>
+          </div>
+  );
+}
+export const DataHandling:React.FC<DataHandlingProps> =({selectOffboadingScope})=> {
   return (
     <section className="bg-white rounded-lg p-6 mb-6">
       <div className="flex flex-col gap-5">
         <h2 className="font-bold text-[19px] text-[#4a4a4a]">Data Handling Summary</h2>
-        <div className="bg-[#f6fdff] border border-[#b0deeb] rounded-lg p-3">
+        {
+          selectOffboadingScope ==="users" && (
+                    <div className="bg-[#f6fdff] border border-[#b0deeb] rounded-lg p-3">
           <div className="flex gap-1.5">
             <div className="w-5 h-5 relative shrink-0">
               <div className="absolute inset-[8.33%]">
@@ -24,6 +45,71 @@ export function DataHandling() {
             </p>
           </div>
         </div>
+          )
+        }
+
+         {
+          selectOffboadingScope ==="project" && (
+            <div>
+         <div className="bg-[#f6fdff] border border-[#b0deeb] rounded-lg p-3">
+          <div className="flex gap-1.5">
+            <div className="w-5 h-5 relative shrink-0">
+              <div className="absolute inset-[8.33%]">
+                <svg
+                  className="absolute block inset-0"
+                  fill="none"
+                  preserveAspectRatio="none"
+                  viewBox="0 0 16.6667 16.6667"
+                >
+                  <path d={svgPaths.p36290900} fill="#006176" />
+                </svg>
+              </div>
+            </div>
+            <p className="font-normal text-[14px] text-[#3b4648] leading-[19px]">
+              Selected data handling actions will be executed once the requset is approved.
+            </p>
+          </div>
+        </div>
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-6">
+          <ToolItem name="Teams Site" status="Archive"  />
+          <ToolItem name="Tool Builder"   status="Delete"  />
+          <ToolItem name="Company Health Check"   status="Archive" />
+        </div>
+        </div>
+          )
+        }
+
+                 {
+          selectOffboadingScope ==="tools" && (
+            <div>
+         <div className="bg-[#f6fdff] border border-[#b0deeb] rounded-lg p-3">
+          <div className="flex gap-1.5">
+            <div className="w-5 h-5 relative shrink-0">
+              <div className="absolute inset-[8.33%]">
+                <svg
+                  className="absolute block inset-0"
+                  fill="none"
+                  preserveAspectRatio="none"
+                  viewBox="0 0 16.6667 16.6667"
+                >
+                  <path d={svgPaths.p36290900} fill="#006176" />
+                </svg>
+              </div>
+            </div>
+            <p className="font-normal text-[14px] text-[#3b4648] leading-[19px]">
+              Data handling actions will be executed only for offboarding tools after approval.
+            </p>
+          </div>
+        </div>
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-6">
+          <ToolItem name="Teams Site" status="Archive"  />
+          <ToolItem name="Company Health Check"   status="Archive" />
+        </div>
+        </div>
+          )
+        }
+
+
       </div>
     </section>
   );
