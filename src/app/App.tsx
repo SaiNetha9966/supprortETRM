ab7852297a37e3484755a8e20b429322a6ab539d
SaiNetha <44888832+SaiNetha9966@users.noreply.github.com>
2026-03-19 23:12:20 +0530

    app changes reverted

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,65 +4,64 @@ import { fetchNonClientNewProject, generateToken, initializeMsalClient } from '.
 import { Loader } from './components/Utils/UiUtilis';
 import DashBoard from './components/Dashboard/DashBoard';
 // import styles from '/../app/App.module.css';
-import styles from '../app/App.module.css';
+import styles from "../app/App.module.css"
 import { Header } from './components/Header/Header';
 
 export default function App() {
   const [accessToken, setAccessToken] = useState<string | null>(null);
   const [nonClientNewProjectData, setNonClientNewProjectData] = useState<any>(null);
-  const [dashBoardType, setDashboardType] = useState<string>('newrequest');
+ const [dashBoardType , setDashboardType] = useState<string>("newrequest");
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  const handleVavigateDashBoard = (dashBoardType: string) => {
-    setDashboardType(dashBoardType);
-  };
-  const toggleSidebar = () => {
+ const handleVavigateDashBoard = (dashBoardType : string) =>{
+  setDashboardType(dashBoardType);
+ }
+   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
-  // useEffect(() => {
-  //   const initializeApp = async (): Promise<void> => {
-  //     try {
-  //       await initializeMsalClient();
+  useEffect(() => {
+    const initializeApp = async (): Promise<void> => {
+      try {
+        await initializeMsalClient();
 
-  //       const token: string | null = await generateToken();
-  //       if (!token) return;
+        const token: string | null = await generateToken();
+        if (!token) return;
 
-  //       setAccessToken(token);
-  //       const projectInfo = await fetchNonClientNewProject(token);
-  //       setNonClientNewProjectData(projectInfo);
-  //       console.log(projectInfo);
-  //     } catch (error) {
-  //       console.error('Error initializing app:', error);
-  //     }
-  //   };
+        setAccessToken(token);
+        const projectInfo = await fetchNonClientNewProject(token);
+        setNonClientNewProjectData(projectInfo);
+        console.log(projectInfo);
+      } catch (error) {
+        console.error('Error initializing app:', error);
+      }
+    };
 
-  //   initializeApp();
-  // }, []);
+    initializeApp();
+  }, []);
 
   // Show loader until token is ready
-  // if (!accessToken) {
-  //   return <Loader />;
-  // }
+  if (!accessToken) {
+    return <Loader />;
+  }
 
   return (
-    <div className={styles.app}>
-      <Header
-        onMenuToggle={toggleSidebar}
-        onNavigateDashBoard={handleVavigateDashBoard}
-        dashBoardType={dashBoardType}
-      />
+       <div className={styles.app}>
+           <Header onMenuToggle={toggleSidebar} onNavigateDashBoard={handleVavigateDashBoard} dashBoardType={dashBoardType} />
 
-      {dashBoardType === 'newrequest' && (
-        <>
-          <MainComponent
-            sidebarOpen={sidebarOpen}
-            setSidebarOpen={setSidebarOpen}
-            nonClientNewProjectData={nonClientNewProjectData}
-            token={accessToken}
-          />
-        </>
-      )}
-      {dashBoardType === 'dashboard' && <DashBoard />}
+                {
+                  dashBoardType === "newrequest" && (
+                    <>
+                    <MainComponent sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} nonClientNewProjectData={nonClientNewProjectData} token={accessToken} />
+
+                    </>
+                  )
+                }
+                {
+                  dashBoardType==="dashboard" && (
+                       <DashBoard/>
+                  )
+                }
+        
     </div>
   );
 }
